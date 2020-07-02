decd9cc9faf65f7954306c5745bac124ad5fc99b
Mario Inga <mario21ic@gmail.com>
2020-07-02 01:00:31 -0500

    updating script

--- ssh_pi.sh
+++ ssh_pi.sh
@@ -2,11 +2,21 @@
 
 CMD=$@
 ips=(192.168.1.200 192.168.1.201 192.168.1.202 192.168.1.203)
+
+# Only for master
+if [[ ( ! -z $1 ) && ( $1 == "master" ) ]]; then
+  ips=(${ips[0]})
+  shift
+  CMD=$@
+fi
+
+# Only for workers
 if [[ ( ! -z $1 ) && ( $1 == "workers" ) ]]; then
   ips=("${ips[@]:1}")
   shift
   CMD=$@
 fi
+
 
 for ip in "${ips[@]}"
 do
