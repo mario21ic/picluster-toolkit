3d4e256052a0fe37bb655e068b784f2f3968264b
Mario Inga <mario21ic@gmail.com>
2020-09-08 01:36:51 -0500

    adding docker multiarch

---
+++ docker/echo/buildx.sh
@@ -0,0 +1,10 @@
+docker run --rm --privileged docker/binfmt:a7996909642ee92942dcd6cff44b9b95f08dad64
+cat /proc/sys/fs/binfmt_misc/qemu-aarch64
+docker buildx ls
+#docker buildx rm mybuilder
+docker buildx create --name mybuilder
+docker buildx use mybuilder
+docker buildx ls
+docker buildx inspect --bootstrap
+#docker buildx build --platform linux/arm,linux/arm64,linux/amd64 -t mario21ic/echo:arm . --push
+docker buildx build --platform linux/arm,linux/arm64,linux/amd64 -t mario21ic/echo:arm . 
